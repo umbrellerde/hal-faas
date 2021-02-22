3ee55e433d167a8368ed4bd43fe5696fbcd4640a
Trever <trever@umbreller.de>
2021-02-22 14:46:45 +0100

    Add basic support for datastore

--- noma/src/main/kotlin/BedrockClient.kt
+++ noma/src/main/kotlin/BedrockClient.kt
@@ -1,10 +1,21 @@
+import com.beust.klaxon.Json
+import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Klaxon
+import com.beust.klaxon.Parser
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
+import java.lang.RuntimeException
 import java.net.Socket
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
 data class InvocationParams(val payload: String)
 
 data class Invocation(val runtime: String, val workload: String, val params: InvocationParams)
+
+@ExperimentalTime
+val runtimeTimeout = 20.seconds
 
 /**
  * Implementation according to https://bedrockdb.com/jobs.html
@@ -43,7 +54,66 @@ class BedrockClient(url: String = "localhost", port: Int = 8888) {
         }
     }
 
-    data class ParsedResponse(val status: Int, val payload: String, val debugInformation: String)
+    /**
+     * create table $name ( $parameters );
+     */
+    fun createTable(name: String, parameters: String) {
+        val message = "Query: create table $name ( $parameters );\n\n"
+        client.getOutputStream().write(message.toByteArray())
+        val res = parseResponse()
+        if (res.status != 200) {
+            throw RuntimeException("Could not create Table. response=$res")
+        }
+    }
+
+    data class DatabaseResponse(val status: Int, val response: JsonObject)
+    /**
+     * select $what from $from;
+     */
+    private fun select(what: String, from: String = "*"): DatabaseResponse {
+        val message = "Query\nquery: select $what from $from;\nformat:json\n\n"
+        client.getOutputStream().write(message.toByteArray())
+        val res = parseResponse()
+        val builder = java.lang.StringBuilder().append(res)
+        return DatabaseResponse(res.status, Klaxon().parser().parse(builder) as JsonObject)
+    }
+
+    data class RuntimeImplementation(val acceleratorType: String, val name: String, val location: String)
+    /**
+     * if there is a free accelerator of type $acceleratorType, get runtimeImplementations it might run
+     */
+    fun getRuntimeImplementation(acceleratorType: String): List<RuntimeImplementation> {
+        val message = "Query\nquery: select name, location from runtime_implementation where accelerator_type == $acceleratorType;\nformat:json\n\n"
+        return listOf(RuntimeImplementation("","",""))
+    }
+
+    fun getRuntimeImplementation(acceleratorType: String, runtime: String): RuntimeImplementation {
+        return RuntimeImplementation("", "", "")
+    }
+
+    data class ImplementationAndInvocation(val inv: Invocation, val runtime: RuntimeImplementation)
+    @ExperimentalTime
+    suspend fun getNextRuntimeAndInvocationToStart(acceleratorType: String): ImplementationAndInvocation {
+        val query_runtime = "Query\nquery: select * from runtime join blah where runtime_impl.accelerator_type == $acceleratorType"
+        val runtimes = listOf("a", "b", "c") // from the query
+        for (runtime in runtimes) {
+            val query_jobs = "Query\nquery: select name from jobs where name GLOB $runtime.*"
+            val res = ""
+            if (res.isNotEmpty()){
+                val inv = consumeInvocation(runtime="runtime", timeout = 5)
+                if (inv.status != 200)
+                    continue
+                return ImplementationAndInvocation(inv.inv, getRuntimeImplementation(acceleratorType, runtime))
+            }
+        }
+        logger.info { "There are no Runtimes that this accelerator can run at the moment. Waiting 20s and trying again" }
+        delay(runtimeTimeout.toLongMilliseconds())
+        return getNextRuntimeAndInvocationToStart(acceleratorType)
+    }
+
+    
+
+    private data class ParsedResponse(val status: Int, val payload: String, val debugInformation: String)
     /**
      * call this instead of reading the response from the socket
      */
--- noma/src/main/kotlin/main.kt
+++ noma/src/main/kotlin/main.kt
@@ -1,9 +1,29 @@
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
+import com.beust.klaxon.Parser
 import mu.KotlinLogging
 
 fun main(args: Array<String>) {
-    val logger = KotlinLogging.logger {}
-    val c = BedrockClient()
-    c.createInvocation(Invocation("onnx", "yolov3", InvocationParams("I have no idea what i'm doing")))
+//    val logger = KotlinLogging.logger {}
+//    val c = BedrockClient()
+//    c.createInvocation(Invocation("onnx", "yolov3", InvocationParams("I have no idea what i'm doing")))
+//
+//    println(c.consumeInvocation(workload = "syolov3"))
+//
+//    c.close()
 
-    println(c.consumeInvocation(workload = "syolov3"))
+//    val jsonString = StringBuilder("""
+//{"headers":["foo","bar"],"rows":[[1,2],[2,3]]}
+//    """)
+//    val parser = Klaxon().parser()
+//    val json = parser.parse(jsonString) as JsonObject
+//    println((json["rows"] as JsonArray<*>)[0])
+
+    val query = """
+        Abc
+        def
+    """.trimIndent() + "\n\n"
+    print(query.toCharArray())
+    print("Fin")
 }
