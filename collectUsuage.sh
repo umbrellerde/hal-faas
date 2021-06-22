daf473b5bcf469f2d9271c2fbedc4603f0ed2d95
umbrellerde <trever@umbreller.de>
2021-06-22 17:36:36 +0200

    Add Final Results (the first batch)

--- collectUsuage.sh
+++ collectUsuage.sh
@@ -2,6 +2,7 @@
 while true; do
     res=$(nvidia-smi --query-gpu=gpu_bus_id,utilization.gpu,utilization.memory --format=csv,noheader,nounits)
     time=$(date +%s%N | cut -b1-13)
-    echo "$time,$res" >> gpuUsuage.csv
+    echo "$time" >> gpuUsuage.csv
+    echo "$res" >> gpuUsuage.csv
     sleep 1s
 done
